43c6fb50a5f89095963327df9d6633ce62c68a50
Serkan Akçay <serkanakcayyy@gmail.com>
2024-11-30 22:31:04 +0300

    List

--- AirbnbClone/Core/Listings/ListingItemView.swift
+++ AirbnbClone/Core/Listings/ListingItemView.swift
@@ -1,5 +1,5 @@
 //
-//  ListingView.swift
+//  ListingItemView.swift
 //  AirbnbClone
 //
 //  Created by serkan on 30.11.2024.
@@ -9,7 +9,39 @@ import SwiftUI
 
 struct ListingItemView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 8){
+            //images
+            Rectangle()
+                .frame(height: 320)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            //Listing Details
+            HStack(alignment: .top){
+                //details
+                VStack(alignment:.leading){
+                    Text("Miami, Florida")
+                        .fontWeight(.semibold)
+                    Text("12 mi away")
+                        .foregroundStyle(.gray)
+                    Text("Now 3 - 10")
+                        .foregroundStyle(.gray)
+                    HStack(spacing:2){
+                        Text("$567")
+                            .fontWeight(.semibold)
+                        Text("night")
+                    }
+                }
+                //Rating
+                Spacer()
+                HStack{
+                    Image(systemName: "star.fill")
+                    Text("4.86")
+                }
+            }
+            .font(.footnote)
+            
+        }
+        .padding()
     }
 }
 
