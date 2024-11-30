4f3f9d8a232282dbbbd833d4795335f245899912
Serkan Akçay <serkanakcayyy@gmail.com>
2024-11-30 22:05:22 +0300

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by serkan on 30.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
