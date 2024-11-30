6772aa637f63bb401884a03dac749f7023587285
Serkan Akçay <serkanakcayyy@gmail.com>
2024-11-30 22:30:06 +0300

    ListingItemView

---
+++ AirbnbClone/Core/Listings/ListingItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingView.swift
+//  AirbnbClone
+//
+//  Created by serkan on 30.11.2024.
+//
+
+import SwiftUI
+
+struct ListingItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingItemView()
+}
