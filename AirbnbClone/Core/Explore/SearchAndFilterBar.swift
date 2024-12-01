434b426272c9603404bd937a83da2f45828f2955
Serkan Akçay <serkanakcayyy@gmail.com>
2024-12-01 20:27:13 +0300

    Search bar filter has been created

---
+++ AirbnbClone/Core/Explore/SearchAndFilterBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchAndFilterBar.swift
+//  AirbnbClone
+//
+//  Created by serkan on 1.12.2024.
+//
+
+import SwiftUI
+
+struct SearchAndFilterBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchAndFilterBar()
+}
