8d739896b166ef7fabcdfcfafc6debf05bda4ab1
Serkan Akçay <serkanakcayyy@gmail.com>
2024-12-02 17:02:56 +0300

    Listing Detail View

---
+++ AirbnbClone/Core/Components/ListinImageCarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  ListinImageCarouselView.swift
+//  AirbnbClone
+//
+//  Created by serkan on 2.12.2024.
+//
+
+import SwiftUI
+
+struct ListinImageCarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListinImageCarouselView()
+}
---
+++ AirbnbClone/Core/Listings/ListinDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ListinDetailView.swift
+//  AirbnbClone
+//
+//  Created by serkan on 2.12.2024.
+//
+
+import SwiftUI
+
+struct ListinDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListinDetailView()
+}
