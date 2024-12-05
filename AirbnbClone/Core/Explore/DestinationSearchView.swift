68b7cbb7f1a930016fb375729710f9094b7265f0
Serkan Akçay <serkanakcayyy@gmail.com>
2024-12-05 21:34:22 +0300

    DestinationSearchView

---
+++ AirbnbClone/Core/Explore/DestinationSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationSearchView.swift
+//  AirbnbClone
+//
+//  Created by serkan on 5.12.2024.
+//
+
+import SwiftUI
+
+struct DestinationSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationSearchView()
+}
